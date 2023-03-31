a9ee2b4015b53ac28c5ac3199248b21a8fee0786
bondik665 <bodarev665566@gmail.com>
2023-03-31 19:56:24 +0300

    first commit

--- js.js
+++ js.js
@@ -1,97 +1,91 @@
-const personDB = {
-    gender:null,
-    age:null,
-    weight:null,
-    height:null,
-    activityCafficient:null,
-    start(){
+ let  gender;
+ const form = document.forms.main;
+ let activityCafficient;
+ const steps = document.querySelectorAll('.step');
+ let age;
+ let weight;
+ let growth;
+
+ //create listenerEVEnt
+  //step 1
+ document.querySelector('.calculator-gender__items').addEventListener('click',(e) =>{
+    document.querySelectorAll('.calculator-gender__item').forEach(i=>{
+        i.classList.remove('active')
+    })
         
-        this.gender = prompt('Укажите ваш пол:1-муж,2-жен')
-        if(this.gender !=='1' && this.gender !=='2'){
-            
-            alert('Ввели неклоректные данные')
-            this.gender = null;
-            this.gender = +prompt('Укажите ваш пол:1-муж,2-жен')
-
-        }
-        
-        this.age = +prompt('Ваш  возраст:');
-        if(check(this.age)){
-            this.age = null;
-            this.age = +prompt('Ваш  возраст:')
-
-        }
-        this.weight = +prompt('Ваш  вес:');
-        if(check(this.weight)){
-            this.weight = null;
-            this.age = +prompt('Ваш  вес:')
-
-        }
-        this.height = +prompt('Вваш  рост:');
-        if(check(this.height)){
-            this.age = null;
-            this.age = +prompt('Вваш  рост:')
-
-        }
-        this.activityCafficient = +prompt('Выберете свой коэф активностbи:1.2 минимальный,1.3-умеренный,1.55-средний,1.7 -высокий,1.9-экстрим')
-        if(this.activityCafficient !==1.2 && this.activityCafficient !==1.3 && this.activityCafficient !==1.55 && this.activityCafficient !==1.7 && this.activityCafficient !==1.9){
-            alert('Ввели неклоректные данные')
-            this.activityCafficient = null;
-            this.activityCafficient = +prompt('Выберете свой коэф активности :1.2 минимальный,1.3-умеренный,1.55-средний,1.7 -высокий,1.9-экстрим');
-
-
-        }
-    },
-    getBMI(){
-        if(!this.weight && !this.height){
-    alert('запустите метод personDB,start()')
-        }
-        const index = +(Math.pow((this.weight/this.height),2)*100).toFixed(2);
-        console.log(index);
-        if(index<18.5){
-            console.log('Ниже нормального веса')
-        }
-        else if (index >= 18.5 && index < 25){
-            console.log('нормальный вес')
-        }
-        else if (index >= 25 && index < 30){
-            console.log('избыточный вес')
-        }
-        else if (index >= 30 && index < 35){
-            console.log('нормальный вес')
-        }
-        else if (index >=35 && index < 45){
-            console.log('Вы жирдяй')
-        }
-        else {
-            console.log('Ахахах')
-        }
-    },
-    getDailyCalories(){
-        if(!this.weight && !this.height && !this.gender && !this.activityCafficient ){
-            alert('запустите метод personDB,start()')
-            }
-
-        if(this.gender === '1'){
-            this.DailyCalories = (10 * this.weight ) +(6.25 * this.height)-(5*this.age) + 5 * this.activityCafficient
-        } 
-
-        else if(this.gender === '2'){
-            this.DailyCalories = (10 * this.weight ) +(6.25 * this.height)-(5*this.age) -161 * this.activityCafficient
-        }  
+    if(e.target.parentElement.closest('.calculator-gender__item')){
+        e.target.parentElement.classList.add('active')
     }
-}    
+    document.querySelector('#step-1-btn').addEventListener('click',(e)=>{
+        e.preventDefault()
+        gender = form.gender.value;
+        steps[0].style.display="none"
+        steps[1].style.display="block"
+        console.log(gender)
 
-
-function check(value){
-    if(value == ''|| value <= 0 || isNaN(value)){
-        alert('Ввели неклоректные данные')
-        return true
+    })
+ })
+ //step 2 create activity
+ document.querySelector('.calculator-activity__items').addEventListener('click',(e)=>{
+    document.querySelectorAll('.calculator-activity__item').forEach(i=>{
+        i.classList.remove('active')
+    })
+    if(e.target.parentElement.closest('.calculator-activity__item')){
+        e.target.parentElement.classList.add('active')
     }
+ })
+
+ document.querySelector('#step-2-btn').addEventListener('click',(e)=>{
+    e.preventDefault()
+    activityCafficient = form.activity.value;
+    steps[1].style.display="none"
+    steps[2].style.display="block"
+        console.log(activityCafficient)
+ })
+ //step 3 enter user value 
+ function check(param){
+    if(param.validity.rangeOverFlow || param.validity.rangeUnderFlow){
+        alert('Вы ввели недопустимое значение')
+    }
+    return false
+
+ }
+form.age.addEventListener('blur',(e)=>{
+if(!check(e.target)){
+    age = e.target.value;
+    }
+})
+form.growth.addEventListener('blur',(e)=>{
+    if(!check(e.target)){
+        growth = e.target.value;
+        }
+    })
+form.weight.addEventListener('blur',(e)=>{
+    if(!check(e.target)){
+        weight = e.target.value;
+        console.log(weight)
+        console.log(growth)
+        console.log(age)
+        }
+    }) ;
+//step 4 == finaly block 
+document.querySelector('#step-result').addEventListener('click',(e)=>{
+    e.preventDefault();
+    steps[2].style.display="none"
+    document.querySelector('.calculator-result').style.display= 'block'
+    const index = +(Math.pow((weight/growth),2)*100).toFixed(2);
+    document.querySelector('#imt-value').innerHTML = index
+    const result = getDailyCalories()
+    document.querySelector('#nc').innerHTML = result;
+});
+
+function getDailyCalories(){
+    if(gender === 'man'){
+        return (10 * weight ) +(6.25 * growth)-(5*age) + 5 * activityCafficient
+    } 
+
+    else{
+         return (10 * weight ) +(6.25 * growth)-(5*age) -161 * activityCafficient
+    }  
 }
-
-//запуск методов
-personDB.start()
-personDB.getBMI()
-personDB.getDailyCalories()
-console.log(personDB)
+;
